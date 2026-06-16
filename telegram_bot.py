d61567896ac533f5e6de07fffd498fb6a7cb9948
kafungto-debug <kafungto@googlemail.com>
2026-06-16 01:07:23 +0100

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,21 @@
+import requests
+
+TELEGRAM_BOT_TOKEN = "7928558624:AAHNQ-wFOeozj3HbI0EE-Fqe96rLPY7ScYI"
+TELEGRAM_CHAT_ID = "6996421827"
+
+
+def send_telegram_message(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+
+    try:
+        r = requests.post(url, data=payload)
+        return r.json()
+    except Exception as e:
+        print("Telegram error:", e)
+        return None
